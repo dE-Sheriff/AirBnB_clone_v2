c318a48c3dd6d4c3af3f61ebb38e8b03c4167436
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-08-01 13:55:52 +0100

    edit on web servers

---
+++ web-01.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
+ssh ubuntu@3.84.168.223
---
+++ web-02.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
+
+ssh ubuntu@100.26.57.196
